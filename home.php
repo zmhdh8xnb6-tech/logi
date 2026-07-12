d1d3b454a187e17176ecc65b179f994626ade734
paulo henrique <phsolucoesemti@gmail.com>
2026-07-11 22:10:19 -0300

    atualizacao1107

--- home.php
+++ home.php
@@ -32,8 +32,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('pendencias')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='pendencias.php'">
-                            <div class="icon">⚠️</div>
+                        <div class="card-servico card-pendencias" onclick="location.href='pendencias.php'">
+                            <div class="icon"><i class="bi bi-exclamation-triangle"></i></div>
                             <h5>Pendências</h5>
                             <p>Resumo do que falta ou venceu</p>
                         </div>
@@ -42,8 +42,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('parcelamentos')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='parcelamentos.php'">
-                            <div class="icon">💰</div>
+                        <div class="card-servico card-parcelamentos" onclick="location.href='parcelamentos.php'">
+                            <div class="icon"><i class="bi bi-cash-coin"></i></div>
                             <h5>Parcelamentos</h5>
                             <p>Acompanhe tributos e dívidas</p>
                         </div>
@@ -52,8 +52,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('financeiro')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='financeiro.php'">
-                            <div class="icon">💳</div>
+                        <div class="card-servico card-financeiro" onclick="location.href='financeiro.php'">
+                            <div class="icon"><i class="bi bi-wallet2"></i></div>
                             <h5>Financeiro</h5>
                             <p>Receitas, contas e cartões</p>
                         </div>
@@ -62,8 +62,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('legalizacao')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='legalizacao.php'">
-                            <div class="icon">🗂️</div>
+                        <div class="card-servico card-legalizacao" onclick="location.href='legalizacao.php'">
+                            <div class="icon"><i class="bi bi-diagram-3"></i></div>
                             <h5>Legalização</h5>
                             <p>Processos, etapas e checklists</p>
                         </div>
@@ -72,8 +72,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('certificados')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='certificados.php'">
-                            <div class="icon">🔐</div>
+                        <div class="card-servico card-certificados" onclick="location.href='certificados.php'">
+                            <div class="icon"><i class="bi bi-shield-lock"></i></div>
                             <h5>Certificado Digital</h5>
                             <p>Controle de certificados digitais</p>
                         </div>
@@ -82,8 +82,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('alvaras')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='alvaras.php'">
-                            <div class="icon">🏢</div>
+                        <div class="card-servico card-alvaras" onclick="location.href='alvaras.php'">
+                            <div class="icon"><i class="bi bi-building"></i></div>
                             <h5>Alvarás</h5>
                             <p>Gerencie licenças e alvarás</p>
                         </div>
@@ -92,8 +92,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('procuracoes')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='procuracoes.php'">
-                            <div class="icon">📜</div>
+                        <div class="card-servico card-procuracoes" onclick="location.href='procuracoes.php'">
+                            <div class="icon"><i class="bi bi-journal-text"></i></div>
                             <h5>Procurações</h5>
                             <p>Controle de autorizações</p>
                         </div>
@@ -102,8 +102,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('paralisacoes')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='paralisadas.php'">
-                            <div class="icon">📂</div>
+                        <div class="card-servico card-paralisacoes" onclick="location.href='paralisadas.php'">
+                            <div class="icon"><i class="bi bi-folder2-open"></i></div>
                             <h5>Paralisações</h5>
                             <p>Gerencie Empresas Paralisadas</p>
                         </div>
@@ -112,8 +112,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('contador')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='contador.php'">
-                            <div class="icon">💼</div>
+                        <div class="card-servico card-contador" onclick="location.href='contador.php'">
+                            <div class="icon"><i class="bi bi-briefcase"></i></div>
                             <h5>Contador</h5>
                             <p>Controle de Inclusão e Exclusão</p>
                         </div>
@@ -122,8 +122,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('baixas')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='baixas.php'">
-                            <div class="icon">📋</div>
+                        <div class="card-servico card-baixas" onclick="location.href='baixas.php'">
+                            <div class="icon"><i class="bi bi-clipboard-check"></i></div>
                             <h5>Baixas</h5>
                             <p>Controle de Baixas</p>
                         </div>
@@ -132,8 +132,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('crf')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='crf.php'">
-                            <div class="icon">📋</div>
+                        <div class="card-servico card-crf" onclick="location.href='crf.php'">
+                            <div class="icon"><i class="bi bi-clipboard2-data"></i></div>
                             <h5>Cadastro CRF</h5>
                             <p>Controle Cadastro FGTS</p>
                         </div>
@@ -142,8 +142,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('contratos')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='prestacao_servico.php'">
-                            <div class="icon">🧾</div>
+                        <div class="card-servico card-contratos" onclick="location.href='prestacao_servico.php'">
+                            <div class="icon"><i class="bi bi-receipt"></i></div>
                             <h5>Contrato de Prestação de Serviços</h5>
                             <p>Controle de Contratos</p>
                         </div>
@@ -152,8 +152,8 @@ exigirLogin();
 
                 <?php if (usuarioPode('usuarios')): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='usuarios.php'">
-                            <div class="icon">👤</div>
+                        <div class="card-servico card-usuarios" onclick="location.href='usuarios.php'">
+                            <div class="icon"><i class="bi bi-person-gear"></i></div>
                             <h5>Usuários e Permissões</h5>
                             <p>Gerencie acessos do sistema</p>
                         </div>
@@ -162,8 +162,8 @@ exigirLogin();
 
                 <?php if (usuarioEhAdmin()): ?>
                     <div class="col-md-4">
-                        <div class="card-servico" onclick="location.href='auditoria.php'">
-                            <div class="icon">🔎</div>
+                        <div class="card-servico card-auditoria" onclick="location.href='auditoria.php'">
+                            <div class="icon"><i class="bi bi-search"></i></div>
                             <h5>Auditoria</h5>
                             <p>Acompanhe ações dos usuários</p>
                         </div>
